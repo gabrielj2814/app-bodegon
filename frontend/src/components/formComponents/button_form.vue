5a1832defb2647954a55b93abec1e9b066bc8c4a
19yisus <jesusitomorales70@gmail.com>
2022-06-30 18:34:26 -0400

    Trabajando en el login

---
+++ frontend/src/components/formComponents/button_form.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+	defineProps<{
+		buttonText: string,
+		buttonClass: string,
+	}>()
+</script>
+<template>
+	<button type="button" :class="buttonClass" class="inline-block px-7 py-3 font-medium text-sm leading-snug 	uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out">
+		Login
+	</button>
+</template>
---
+++ frontend/src/components/formComponents/checkbox_form.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+	defineProps<{
+		idInput: string,
+		labelText: string,
+		labelClass: string
+	}>()
+</script>
+
+<template>
+	<div class="form-group form-check">
+	   <input type="checkbox" class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+		:id="idInput"/>
+	 <label :class="labelClass" class="form-check-label inline-block text-gray-800" :for="idInput">{{ labelText }}</label>
+	</div>
+</template>
