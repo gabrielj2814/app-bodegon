deece896197145876aaf8ae9be296e1e98c9b277
gabriel <gabrielj2814@hotmail.com>
2022-07-01 16:14:34 -0400

    agregando ficheros para borrar y resetiar las base de datos

---
+++ backend/sql/delete_tables.sql
@@ -0,0 +1,9 @@
+DELETE FROM ttipo_personal;
+DELETE FROM tpersona;
+DELETE FROM tperfil;
+DELETE FROM tmodulo;
+DELETE FROM tsub_modulo;
+DELETE FROM tpregunta_seguridad;
+DELETE FROM tpersonal;
+DELETE FROM timagen_personal;
+DELETE FROM tpregunta_seguridad_personal;
--- backend/sql/reset_db_table.sql
+++ backend/sql/reset_db_table.sql
@@ -1,2 +1,11 @@
 -- consulta para reiniciar el indice de un campo serial
 -- ALTER SEQUENCE <tabla>_<id>_seq RESTART WITH 1
+ALTER SEQUENCE ttipo_personal_id_tipo_personal_seq RESTART WITH 1
+ALTER SEQUENCE tpersona_id_persona_seq RESTART WITH 1
+ALTER SEQUENCE tperfil_id_perfil_seq RESTART WITH 1
+ALTER SEQUENCE tmodulo_id_modulo_seq RESTART WITH 1
+ALTER SEQUENCE tsub_modulo_id_sub_modulo_seq RESTART WITH 1
+ALTER SEQUENCE tpregunta_seguridad_id_pregunta_seguridad_seq RESTART WITH 1
+ALTER SEQUENCE tpersonal_id_personal_seq RESTART WITH 1
+ALTER SEQUENCE timagen_personal_id_imagen_personal_seq RESTART WITH 1
+ALTER SEQUENCE tpregunta_seguridad_personal_id_pregunta_seguridad_personal_seq RESTART WITH 1
